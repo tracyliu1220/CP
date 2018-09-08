b5c014214616f0655d9fb71aa737460dd8ddde65
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2018-09-08 11:53:24 +0800

    hackerrank

--- Hackerrank/2.cpp
+++ Hackerrank/2.cpp
@@ -3,58 +3,81 @@ using namespace std;
 
 int n, m, a, b;
 
+/*
 struct node {
 	int p;
 	int a = 0;
+	bool operator<(node n1, node n2) {
+		if (n1.a < n2.a) return true;
+		else return false;
+	}
 };
+*/
 
-node nodes[300005];
-vector<vector<node> > aj[300005];
+pair<int, int> nodes[300005]; //first:a, second:p
+vector<int> aj[300005];
 
 int find(int x) {
 	int _x = x;
-	while (_x != nodes[_x].p) _x = find(nodes[_x].p);
-	return nodes[x].p = _x;
+	while (_x != nodes[_x].second) _x = find(nodes[_x].second);
+	return nodes[x].second = _x;
 }
 
+/*
 bool compare(node a, node b) {
 	if (a.a < b.a) return true;
 	else return false;
 }
+*/
 
 int main() {
 
 	cin >> n >> m >> a >> b;
 
 	for (int i = 0; i < n; i ++) 
-		nodes[i].p = i;
+		nodes[i].second = i;
 
 	int u, v;
 	for (int i = 0; i < m; i ++) {
 		cin >> u >> v;
+		u--; v--;
 		int pu = find(u), pv = find(v);
-		nodes[pv].p = pu;
+		nodes[pv].second = pu;
+		nodes[u].first ++;
+		nodes[v].first ++;
 	}
-	
-	node N;
 
 	for (int i = 0; i < n; i ++) {
 		int p = find(i);
-		aj[p].push_back(nodes[i]);
+		int tmp = nodes[i].first;
+		aj[p].push_back(tmp);
 	}
 
 	int cnt = 0;
 	int L, tar;
-	vector<int>::iterator R;
+	//vector<int>::iterator R;
+	int R;
 
 	for (int i = 0; i < n; i ++) {
-		if (!aj[i].size()) break;
+		/*
+		cout << "*" << aj[i].size() << endl;
+		for (int j = 0; j < aj[i].size(); j ++)
+			cout << aj[i][j] << " ";
+		cout << endl;
+		*/
+		if (!aj[i].size()) continue;
 		sort(aj[i].begin(), aj[i].end());
-		L = aj[i].begin()->a * a;
-		R = lower_bound(aj[i].begin(), aj[i].end(), L / b);
-		if (R == aj[i].end()) continue;
-		tar = (int)(R - aj[i].begin());
-		cnt += tar + 1;
+
+		L = *(aj[i].begin()) * a;
+		R = *(aj[i].end() - 1) * b;
+
+		for (int j = 1; j < aj[i].size(); j ++)
+			if (L < aj[i][j] && aj[i][j] < R) cnt ++;
+		//cout << "L = " << L << endl;
+		//R = upper_bound(aj[i].begin(), aj[i].end(), );
+		//if (R == aj[i].end()) continue;
+		//tar = (int)(aj[i].end() - R);
+		//cnt += tar + 1;
 	}
 
 	cout << cnt << endl;
