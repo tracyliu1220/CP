e03fad4005bbb01cfaee7b9c8665ba370645d355
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2018-10-14 01:23:06 +0800

    POJ

--- PCCA/omega/Ch3/uva_10684.cpp
+++ PCCA/omega/Ch3/uva_10684.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 10005
+typedef long long LL;
 
 int n;
 
 int main() {
 
-	while (cin >> n && n) {
-		
-		int Max = 0;
-		int sum = 0;
-		int current;
+	cin >> n;		
 
-		for (int i = 0; i < n; i ++) {
-			cin >> current;
-			if (sum < 0) sum = current;
-			else sum += current;
-			Max = max(Max, sum);
-		}
-		if (Max) cout << "The maximum winning streak is " << Max << "." << endl;
-		else cout << "Losing streak." << endl;
+	LL Max = 0;
+	LL sum = 0;
+	int current;
 
+	for (int i = 0; i < n; i ++) {
+		cin >> current;
+		if (sum < 0) sum = current;
+		else sum += current;
+		Max = max(Max, sum);
 	}
-
+	
+	cout << Max << endl;	
 }
--- PCCA/omega/Ch4/uva_00315.cpp
+++ PCCA/omega/Ch4/uva_00315.cpp
@@ -1,6 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+
+int G[105][105];
+int dfn[105];
+int low[105];
+int vis[105];
+
+void dfs(int u) {
+	int child = 0;
+	int cnt = 0;
+
+	dfn[u] = low[u] = ++cnt;
+
+	stack<int> st; st.push(u);
+
+	while (st.size()) {
+		int cur = st.top(); st.pop();
+		if (!vis[cur]) {
+			child ++;
+			p[cur] = u;
+
+
+		}
+	}
+}
+
 int main() {
-	
+
+	while (cin >> n && n) { //1 base!!
+		
+		memset(G, 0, sizeof(G));
+		memset(vis, 0, sizeof(vis));
+		string str;
+		
+		while (getline(cin, str) && str[0] != '0') {
+			stringstream ss(str);
+			int a, b;
+			ss >> a;
+			while (ss >> b) {
+				G[a][b] = G[b][a] = 1;
+			}
+		}
+
+	}
+
+
 }
