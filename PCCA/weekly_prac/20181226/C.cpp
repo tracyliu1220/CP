311e497c72482aeaffe2b3a5c69f61816da6fe41
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2018-12-23 21:43:53 +0800

    indentation fixed

--- PCCA/weekly_prac/20181226/C.cpp
+++ PCCA/weekly_prac/20181226/C.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main(){
-	
-	int x, y, _x, _y;
+    
+    int x, y, _x, _y;
     int T;
-	
-	while(cin >> T && T){
-		
-		cin >> x >> y;
-		while(T --){
-			
-			cin >> _x >> _y;
-			
-			if(_x - x > 0 && _y - y > 0)      cout << "NE\n";
-			else if(_x - x < 0 && _y - y > 0) cout << "NO\n";
-			else if(_x - x > 0 && _y - y < 0) cout << "SE\n";
-			else if(_x - x < 0 && _y - y < 0) cout << "SO\n";
-			else cout << "divisa\n";
-		}
-	}
+    
+    while(cin >> T && T){
+        
+        cin >> x >> y;
+        while(T --){
+            
+            cin >> _x >> _y;
+            
+            if(_x - x > 0 && _y - y > 0)      cout << "NE\n";
+            else if(_x - x < 0 && _y - y > 0) cout << "NO\n";
+            else if(_x - x > 0 && _y - y < 0) cout << "SE\n";
+            else if(_x - x < 0 && _y - y < 0) cout << "SO\n";
+            else cout << "divisa\n";
+        }
+    }
 }
